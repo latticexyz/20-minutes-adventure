081f225d50d49959935f7b06151845d794571e80
alvrs <alvarius@lattice.xyz>
2023-11-12 22:44:09 +0300

    render GameMap

--- grassland/packages/client/src/App.tsx
+++ grassland/packages/client/src/App.tsx
@@ -1,3 +1,18 @@
+import { GameMap } from "./GameMap";
+import { useMUD } from "./MUDContext";
+
 export const App = () => {
-  return <></>;
+  const {
+    network: { useStore, tables },
+  } = useMUD();
+
+  const tiles = useStore((store) => {
+    return Object.values(store.getRecords(tables.Position)).map((record) => ({
+      x: record.value.x,
+      y: record.value.y,
+      emoji: "👩‍🔬",
+    }));
+  });
+
+  return <GameMap width={16} height={16} tiles={tiles} />;
 };
