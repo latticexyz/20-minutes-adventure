1a6f872fca5e882881fac4ace329fe277d954c3d
alvrs <alvarius@lattice.xyz>
2023-11-17 18:39:31 +0300

    add out

---
+++ grassland/packages/contracts/out/IMoveSystem.sol/IMoveSystem.abi.json.d.ts
@@ -0,0 +1,20 @@
+declare const abi: [
+  {
+    "inputs": [
+      {
+        "internalType": "uint32",
+        "name": "x",
+        "type": "uint32"
+      },
+      {
+        "internalType": "uint32",
+        "name": "y",
+        "type": "uint32"
+      }
+    ],
+    "name": "grassland_MoveSystem_move",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]; export default abi;
